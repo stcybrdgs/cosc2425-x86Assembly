ce897800e4d0de477df12fc1cfa1f107b0549b9a
Stacy Bridges <stcybrdgs@users.noreply.github.com>
2015-08-31 09:43:08 -0500

    Update countedWhile_nasm.asm

--- lab3/src/countedWhile_nasm.asm
+++ lab3/src/countedWhile_nasm.asm
@@ -6,27 +6,27 @@
     section .text
 main:
     ; function setup
-    push        RBP
-    mov         RBP, RSP
-    sub         RSP, 16
+    push    RBP
+    mov     RBP, RSP
+    sub     RSP, 16
 
 loop:
-    cmp         byte [primRead], 5  ; compare sentinel to priming read
-                                    ; to see if they are equal
-                                    ; run loop while test condition is false
-    je          exit_script         ; if test condtion is true, exit program
+    cmp     byte [primRead], 5  ; compare sentinel to priming read
+                                ; to see if they are equal
+                                ; run loop while test condition is false
+    je      exit_script         ; if test condtion is true, exit program
 
-    lea         RDI, [rel print_it] ; print the count stored in primRead
-    mov         AL, 0
-    call        printf
-    inc         byte [primRead]     ; increment primRead counter
-    jmp         loop                ; run the loop again
+    lea     RDI, [rel print_it] ; print the count stored in primRead
+    mov     AL, 0
+    call    printf
+    inc     byte [primRead]     ; increment primRead counter
+    jmp     loop                ; run the loop again
 
 exit_script:
     ; function return
-    mov         RAX, 0
-    add         RSP, 16
-    pop         RBP
+    mov     RAX, 0
+    add     RSP, 16
+    pop     RBP
 
     ret
 
